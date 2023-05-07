43d6806f0f90a3d0e9f31a04b77facd8e4df9764
João Vitor Araújo de Jesus <joaovitoraj@poli.ufrj.br>
2023-05-07 16:23:06 -0300

    Imprime o maior.py

---
+++ Imprime o maior.py
@@ -0,0 +1,14 @@
+"""
+Faça um Programa que peça dois números e imprima o maior deles.
+"""
+
+
+
+num1 = input("Insira o primeiro número: ")
+num2 = input("Insira o segundo número: ")
+
+if num1 >= num2:
+    print (num1)
+    
+else:
+    print (num2)
