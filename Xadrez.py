36b3bb3cc0e7618c44b66f2c7f33939c73f4a770
João Vitor Araújo de Jesus <joaovitoraj@poli.ufrj.br>
2023-05-23 22:15:21 -0300

    Xadrez.py

---
+++ Xadrez.py
@@ -0,0 +1,12 @@
+l=int(input())
+c=int(input())
+if c%2 ==0:
+    if l%2 != 0:
+        print("0")
+    else:
+        print("1")
+else:
+    if l%2==0:
+        print("0")
+    else:
+        print("1")
